8c16107c65711d8afad63f017b5e39dec148df20
Zack Buhman <zack@buhman.org>
2023-07-21 00:36:42 +0000

    main.s: add DISPLAY_ON

--- main.s
+++ main.s
@@ -2,8 +2,9 @@
 
         mov r0, #0x4000000 /* IO_REG */
         mov r1, #0x0
-        orr r1, r1, #(1 << 7) /* DISPCNT__FORCED_BLANK */
-        strh r1, [r0, #0x000] /* DISPCNT */
+        //orr r1, r1, #(1 << 7) /* DISPCNT__FORCED_BLANK */
+	orr r1, r1, #(1 << 16) /* DISPCNT__DISPLAY_ON */
+        str r1, [r0, #0x000] /* DISPCNT */
 
 forever9:
 	mov r1, #0x9
